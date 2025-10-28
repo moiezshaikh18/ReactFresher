1f91570e3b73f2dac0a66bbff1eb91dd4b11e5b6
mueej <v-mueej@fieldera.com>
2025-10-28 20:59:21 +0530

    clean up

--- Projects/1-todo-app context/src/App.jsx
+++ Projects/1-todo-app context/src/App.jsx
@@ -2,49 +2,15 @@ import AppName from "./Components/AppName/AppName";
 import AddTodo from "./Components/AddTodo";
 import { TodoItems } from "./Components/TodoItems";
 import "./App.css";
-import { useReducer } from "react";
-import { TodoItemsContext } from "./store/todoItemStore";
-
-const reducerHandler = (state, action) => {
-  switch (action.type) {
-    case "AddItem":
-      return [...state, action.payload.todo];
-
-    case "DeleteItem":
-      return state.filter((item) => item.id !== action.payload.id); // Delete todo
-
-    default:
-      return state; // always return current state as fallback
-  }
-};
+import { TodoItemsContextProvider } from "./store/todoItemStore";
 
 function App() {
-  const [todos, dispatch] = useReducer(reducerHandler, []);
-
-  const handleAddTodo = (todo) => {
-    dispatch({ type: "AddItem", payload: { todo } });
-  };
-
-  const handleDeleteTodo = (id) => {
-    dispatch({ type: "DeleteItem", payload: { id } });
-  };
-
-  console.log(todos);
-
   return (
-    <TodoItemsContext.Provider
-      value={{
-        todos,
-        handleAddTodo,
-        handleDeleteTodo,
-      }}
-    >
-      <div>
-        <AppName />
-        <AddTodo />
-        <TodoItems />
-      </div>
-    </TodoItemsContext.Provider>
+    <TodoItemsContextProvider>
+      <AppName />
+      <AddTodo />
+      <TodoItems />
+    </TodoItemsContextProvider>
   );
 }
 
--- Projects/1-todo-app context/src/store/todoItemStore.jsx
+++ Projects/1-todo-app context/src/store/todoItemStore.jsx
@@ -1,7 +1,47 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const TodoItemsContext = createContext({
   todos: [],
   handleAddTodo: () => {},
   handleDeleteTodo: () => {},
 });
+
+const reducerHandler = (state, action) => {
+  switch (action.type) {
+    case "AddItem":
+      return [...state, action.payload.todo];
+
+    case "DeleteItem":
+      return state.filter((item) => item.id !== action.payload.id); // Delete todo
+
+    default:
+      return state; // always return current state as fallback
+  }
+};
+
+export const TodoItemsContextProvider = ({ children }) => {
+  const [todos, dispatch] = useReducer(reducerHandler, []);
+
+  const handleAddTodo = (todo) => {
+    dispatch({ type: "AddItem", payload: { todo } });
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch({ type: "DeleteItem", payload: { id } });
+  };
+
+  console.log(todos);
+  return (
+    <>
+      <TodoItemsContext.Provider
+        value={{
+          todos,
+          handleAddTodo,
+          handleDeleteTodo,
+        }}
+      >
+        {children}
+      </TodoItemsContext.Provider>
+    </>
+  );
+};

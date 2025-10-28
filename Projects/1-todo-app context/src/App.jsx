08c0498eeb8f0cc802bceb2ac29ebfde82e0a7c7
mueej <v-mueej@fieldera.com>
2025-10-28 16:56:57 +0530

    using useReducer

--- Projects/1-todo-app context/src/App.jsx
+++ Projects/1-todo-app context/src/App.jsx
@@ -2,18 +2,31 @@ import AppName from "./Components/AppName/AppName";
 import AddTodo from "./Components/AddTodo";
 import { TodoItems } from "./Components/TodoItems";
 import "./App.css";
-import { useState } from "react";
+import { useReducer } from "react";
 import { TodoItemsContext } from "./store/todoItemStore";
 
+const reducerHandler = (state, action) => {
+  switch (action.type) {
+    case "AddItem":
+      return [...state, action.payload.todo];
+
+    case "DeleteItem":
+      return state.filter((item) => item.id !== action.payload.id); // Delete todo
+
+    default:
+      return state; // always return current state as fallback
+  }
+};
+
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, dispatch] = useReducer(reducerHandler, []);
 
   const handleAddTodo = (todo) => {
-    setTodos((prev) => [...prev, todo]);
+    dispatch({ type: "AddItem", payload: { todo } });
   };
 
   const handleDeleteTodo = (id) => {
-    setTodos((prev) => prev.filter((item) => item.id !== id));
+    dispatch({ type: "DeleteItem", payload: { id } });
   };
 
   console.log(todos);
